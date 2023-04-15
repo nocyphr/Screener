c5752e816905f88664100039a95e70694f0106b6
nocyphr <nocphyr@outlook.com>
2023-04-15 12:20:40 +0200

    gherkin step definitions, first outline

---
+++ features/steps/stock_screener_frontend.js
@@ -0,0 +1,56 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('I am on the Stock Management React App', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I should see the title {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('I see the filter section', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('I input a filter criteria in the filter section', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I should see the table of stocks update with the filtered results', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('I see the table of stocks', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When(/^I click on a column with (.*?) order$/, function (current_sorting) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then(/^I should see the table of stocks sorted by the selected column in (.*?) order$/, function (new_sorting) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('I see the {string} button', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('I click the {string} button', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I receive the current state of the displayed dataset in csv-format', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
