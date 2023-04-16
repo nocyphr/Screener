8a5eddb57a2a91e897cb069f6a09e4a5e2bee5a4
nocyphr <nocyphr@outlook.com>
2023-04-16 15:08:16 +0200

    set initial state,  no render without set state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from './components/Container';
 import Header from './components/Header';
+import Filter from './components/Filter';
+import TableView from './components/TableView';
 
 const App = () => {
   const headerTitle = 'Nocyphr Stock Screener';
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setFilteredData(data);
+        setColumns(Object.keys(data[0]));
+      });
+  }, []);
+
+  const handleApplyFilters = (filteredData) => {
+    setFilteredData(filteredData);
+  };
 
   return (
     <Container>
       <Header title={headerTitle} />
-      {/* Your other components go here */}
+      {columns.length > 0 && (
+        <Filter data={data} columns={columns} onApplyFilters={handleApplyFilters} />
+      )}
+      <TableView data={filteredData} columns={columns} />
     </Container>
   );
 };
