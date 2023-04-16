45e9c90cab85ebc787ab97d081b79b8508237b97
nocyphr <nocyphr@outlook.com>
2023-04-16 15:15:56 +0200

    Added sorting functionality

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TableView = ({ data, columns }) => {
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'none' });
+
+  const handleHeaderClick = (column) => {
+    let direction = 'ascending';
+    if (sortConfig.key === column && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key: column, direction });
+  };
+
+  const sortedData = React.useMemo(() => {
+    const sorted = [...data];
+    if (sortConfig.key && sortConfig.direction !== 'none') {
+      sorted.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sorted;
+  }, [data, sortConfig]);
+
   return (
     <table className="w-full text-gray-400 mt-8">
       <thead>
         <tr>
           {columns.map((column, index) => (
-            <th key={index} className="text-left">
+            <th
+              key={index}
+              className="text-left cursor-pointer"
+              onClick={() => handleHeaderClick(column)}
+            >
               {column}
+              {sortConfig.key === column && (
+                <span className="inline-block ml-1">
+                  {sortConfig.direction === 'ascending' ? '▲' : '▼'}
+                </span>
+              )}
             </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {data.map((item, index) => (
+        {sortedData.map((item, index) => (
           <tr key={index} className="border-t border-gray-600">
             {columns.map((column, colIndex) => (
               <td key={colIndex} className={colIndex === 0 ? 'py-2' : ''}>
