8b351718944a1aede1e98d966277faddb4bba71f
nocyphr <nocyphr@outlook.com>
2023-04-16 15:19:33 +0200

    Added pagination

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 const TableView = ({ data, columns }) => {
   const [sortConfig, setSortConfig] = useState({ key: null, direction: 'none' });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
 
   const handleHeaderClick = (column) => {
     let direction = 'ascending';
@@ -27,38 +29,85 @@ const TableView = ({ data, columns }) => {
     return sorted;
   }, [data, sortConfig]);
 
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleItemsPerPageChange = (e) => {
+    setItemsPerPage(parseInt(e.target.value, 10));
+    setCurrentPage(1);
+  };
+
+  const paginatedData = sortedData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const totalPages = Math.ceil(sortedData.length / itemsPerPage);
+
   return (
-    <table className="w-full text-gray-400 mt-8">
-      <thead>
-        <tr>
-          {columns.map((column, index) => (
-            <th
-              key={index}
-              className="text-left cursor-pointer"
-              onClick={() => handleHeaderClick(column)}
-            >
-              {column}
-              {sortConfig.key === column && (
-                <span className="inline-block ml-1">
-                  {sortConfig.direction === 'ascending' ? '▲' : '▼'}
-                </span>
-              )}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {sortedData.map((item, index) => (
-          <tr key={index} className="border-t border-gray-600">
-            {columns.map((column, colIndex) => (
-              <td key={colIndex} className={colIndex === 0 ? 'py-2' : ''}>
-                {item[column]}
-              </td>
+    <>
+      <table className="w-full text-gray-400 mt-8">
+        <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th
+                key={index}
+                className="text-left cursor-pointer"
+                onClick={() => handleHeaderClick(column)}
+              >
+                {column}
+                {sortConfig.key === column && (
+                  <span className="inline-block ml-1">
+                    {sortConfig.direction === 'ascending' ? '▲' : '▼'}
+                  </span>
+                )}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {paginatedData.map((item, index) => (
+            <tr key={index} className="border-t border-gray-600">
+              {columns.map((column, colIndex) => (
+                <td key={colIndex} className={colIndex === 0 ? 'py-2' : ''}>
+                  {item[column]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="flex justify-end items-center mt-4">
+        <span className="text-gray-400 mr-2">Items per page:</span>
+        <select
+          className="bg-gray-700 text-gray-400 p-2 rounded mr-4"
+          value={itemsPerPage}
+          onChange={handleItemsPerPageChange}
+        >
+                    <option value={10}>10</option>
+          <option value={20}>20</option>
+          <option value={50}>50</option>
+        </select>
+        <button
+          className="bg-gray-600 text-gray-200 p-2 rounded mr-1"
+          disabled={currentPage === 1}
+          onClick={() => handlePageChange(currentPage - 1)}
+        >
+          Previous
+        </button>
+        <span className="text-gray-400 mr-1">
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          className="bg-gray-600 text-gray-200 p-2 rounded"
+          disabled={currentPage === totalPages}
+          onClick={() => handlePageChange(currentPage + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 };
 
