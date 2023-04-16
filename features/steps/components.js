94d7c9cfa06056dd92483a49fdf0b799e0d9f2bb
nocyphr <nocphyr@outlook.com>
2023-04-16 09:11:29 +0200

    tests are still failing, outline is undefined although it exists -> regex?

---
+++ features/steps/components.js
@@ -0,0 +1,34 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { By, Key, until } = require('selenium-webdriver');
+const assert = require('assert');
+const { getDriver } = require('./common'); // Make sure the path is correct to the common.js file
+
+Then('I should see the title {string}', async function (string) {
+    const driver = getDriver();
+    const title = await driver.findElement(By.css('h1')).getText();
+    assert.strictEqual(title, string);
+});
+
+Then('I should see the table component', async function () {
+    const driver = getDriver();
+    const table = await driver.findElement(By.css('table')); // Replace with the correct CSS selector for the table
+    assert.ok(table);
+});
+
+Then('I should see the download button component', async function () {
+    const driver = getDriver();
+    const downloadButton = await driver.findElement(By.css('#download-button')); // Replace with the correct CSS selector for the download button
+    assert.ok(downloadButton);
+});
+
+Then('I should see the {string} column', async function (column) {
+    const driver = getDriver();
+    const columnHeader = await driver.findElement(By.xpath(`//table//th[contains(text(), "${column}")]`));
+    assert.ok(columnHeader);
+});
+
+Then('I should see the filter section component', async function () {
+    const driver = getDriver();
+    const filterSection = await driver.findElement(By.css('#filter-section')); // Replace with the correct CSS selector for the filter section
+    assert.ok(filterSection);
+});
