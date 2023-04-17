9137280558184747253394fb796695360b290790
nocyphr <nocyphr@outlook.com>
2023-04-17 07:24:54 +0200

    first try at capability tests. in need of refinement

--- features/steps/stock_screener_frontend.js
+++ features/steps/stock_screener_frontend.js
@@ -1,50 +1,55 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const { By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
-const { driver } = require('./common'); // Update the path to the hooks.js file if needed
+const { getDriver } = require('./common');
 
-
-Given('I see the filter section', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Given('I see the filter section', async function () {
+  const driver = getDriver();
+  const filterSection = await driver.findElement(By.css('#filter-section'));
+  assert.ok(filterSection);
+  // clean up: no assert here please, use context to pass on the FilterSection and continue in WHEN block
 });
 
-When('I input a filter criteria in the filter section', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+// this needs to be refined in the Feature file -> Outline with examples column | filter -> test text column, numerical column, operators
+When('I input a filter criteria in the filter section', async function () {
+  const driver = getDriver();
+  const filterInput = await driver.findElement(By.css('#filter-section input'));
+  await filterInput.sendKeys('A', Key.RETURN);
 });
 
-Then('I should see the table of stocks update with the filtered results', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('I should see the table of stocks update with the filtered results', async function () {
+  // e.g. Test that there are no results not containing "A"
 });
 
-Given('I see the table of stocks', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Given('I see the table of stocks', async function () {
+  const driver = getDriver();
+  const table = await driver.findElement(By.css('table'));
+  assert.ok(table);
 });
 
-When(/^I click on a column with (.*?) order$/, function (current_sorting) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+When(/^I click on a column with (.*?) order$/, async function (current_sorting) {
+  const driver = getDriver();
+  const columnHeader = await driver.findElement(By.xpath(`//table//thead//tr//th[contains(text(), "${current_sorting}")]`));
+  await columnHeader.click();
 });
 
-Then(/^I should see the table of stocks sorted by the selected column in (.*?) order$/, function (new_sorting) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then(/^I should see the table of stocks sorted by the selected column in (.*?) order$/, async function (new_sorting) {
+  // Implement the verification of the sorted table
 });
 
-Given('I see the {string} button', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Given('I see the {string} button', async function (string) {
+  const driver = getDriver();
+  const downloadButton = await driver.findElement(By.css('#download-button'));
+  // clean up: no assert in given, use context to pass on to WHEN block
+  assert.ok(downloadButton);
 });
 
-When('I click the {string} button', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+When('I click the {string} button', async function (string) {
+  const driver = getDriver();
+  const downloadButton = await driver.findElement(By.css('#download-button'));
+  await downloadButton.click();
 });
 
-Then('I receive the current state of the displayed dataset in csv-format', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('the dataset is downloaded in csv-format', function () {
+  // Manual testing?
 });
