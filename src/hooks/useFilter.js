199013dac5057b694f07a69caa617e768c0f5f42
nocyphr <nocyphr@outlook.com>
2023-04-18 12:30:20 +0200

    combined into useFilter hook

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,15 +1,72 @@
 import { useState } from 'react';
-import {
-  applyFilters,
-  handleApplyFilter,
-  handleRemoveFilter,
-  handleClearFilters,
-} from '../helpers/FilterLogic';
 
 const useFilter = (data, columns, onApplyFilters, initialFilter) => {
   const [selectedFilter, setSelectedFilter] = useState(initialFilter || columns[0]);
   const [filterValue, setFilterValue] = useState('');
   const [appliedFilters, setAppliedFilters] = useState([]);
+
+  const applyFilters = (data, appliedFilters, columns) => {
+    return appliedFilters.reduce((filteredData, filter) => {
+      return filteredData.filter((item) => {
+        const operator = filter.value[0];
+        const filterValue = filter.value.slice(1).trim();
+
+        switch (operator) {
+          case '>':
+            return parseFloat(item[filter.type]) > parseFloat(filterValue);
+          case '<':
+            return parseFloat(item[filter.type]) < parseFloat(filterValue);
+          case '!':
+            return !item[filter.type].toString().toLowerCase().includes(filterValue.toLowerCase());
+          default:
+            if (columns.includes(filter.type)) {
+              return item[filter.type].toString().toLowerCase().includes(filter.value.toLowerCase());
+            } else {
+              return true;
+            }
+        }
+      });
+    }, data);
+  };
+
+  const handleApplyFilter = (
+    selectedFilter,
+    filterValue,
+    appliedFilters,
+    setAppliedFilters,
+    setFilterValue,
+    data,
+    onApplyFilters,
+    columns
+  ) => () => {
+    if (filterValue.trim() !== '') {
+      const camelCaseType = selectedFilter.charAt(0).toUpperCase() + selectedFilter.slice(1);
+      const newFilter = { type: camelCaseType, value: filterValue };
+      const newAppliedFilters = [...appliedFilters, newFilter];
+      setAppliedFilters(newAppliedFilters);
+      setFilterValue('');
+
+      const newData = applyFilters(data, newAppliedFilters, columns);
+      onApplyFilters(newData);
+    }
+  };
+
+  const handleRemoveFilter = (appliedFilters, setAppliedFilters, data, onApplyFilters, columns) => (
+    index
+  ) => {
+    const newAppliedFilters = appliedFilters.filter(
+      (_, filterIndex) => filterIndex !== index
+    );
+    setAppliedFilters(newAppliedFilters);
+
+    const newData = applyFilters(data, newAppliedFilters, columns);
+    onApplyFilters(newData);
+  };
+
+  const handleClearFilters = (setAppliedFilters, onApplyFilters, data) => () => {
+    setAppliedFilters([]);
+    onApplyFilters(data);
+  };
 
   const handleFilterChange = (e) => {
     setSelectedFilter(e.target.value);
