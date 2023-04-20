0c95913967da82386634d1d0d612f46acefeb94b
nocyphr <nocyphr@outlook.com>
2023-04-20 08:25:53 +0200

    added error handling if data can't be fetched fail gracefully

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -4,18 +4,37 @@ const useFetchData = (url) => {
   const [data, setData] = useState([]);
   const [columns, setColumns] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    fetch(url, { signal })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network error occurred');
+        }
+        return response.json();
+      })
       .then((data) => {
         setData(data);
         setColumns(Object.keys(data[0]));
         setLoading(false);
+      })
+      .catch((error) => {
+        if (error.name !== 'AbortError') {
+          setError(error.message);
+          setLoading(false);
+        }
       });
+
+    return () => {
+      abortController.abort();
+    };
   }, [url]);
 
-  return { data, columns, loading };
+  return { data, columns, loading, error };
 };
 
 export default useFetchData;
