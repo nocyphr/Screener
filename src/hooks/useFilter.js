15452441f8a3f1dd13e1d1435bbf3dc34136512e
nocyphr <nocyphr@outlook.com>
2023-04-20 13:13:26 +0200

    extracted applyFilters function to utils.js

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,28 +1,5 @@
 import { useState, useCallback } from 'react';
-
-const applyFilters = (data, appliedFilters, columns) => {
-  return appliedFilters.reduce((filteredData, filter) => {
-    return filteredData.filter((item) => {
-      const operator = filter.value[0];
-      const filterValue = filter.value.slice(1).trim();
-
-      switch (operator) {
-        case '>':
-          return parseFloat(item[filter.type]) > parseFloat(filterValue);
-        case '<':
-          return parseFloat(item[filter.type]) < parseFloat(filterValue);
-        case '!':
-          return !item[filter.type].toString().toLowerCase().includes(filterValue.toLowerCase());
-        default:
-          if (columns.includes(filter.type)) {
-            return item[filter.type].toString().toLowerCase().includes(filter.value.toLowerCase());
-          } else {
-            return true;
-          }
-      }
-    });
-  }, data);
-};
+import { applyFiltersToData } from '../utils';
 
 const useFilter = (data, columns, onApplyFilters, initialFilter) => {
   const [selectedFilter, setSelectedFilter] = useState(initialFilter || columns[0]);
@@ -37,7 +14,7 @@ const useFilter = (data, columns, onApplyFilters, initialFilter) => {
       setAppliedFilters(newAppliedFilters);
       setFilterValue('');
 
-      const newData = applyFilters(data, newAppliedFilters, columns);
+      const newData = applyFiltersToData(data, newAppliedFilters, columns);
       onApplyFilters(newData);
     }
   }, [selectedFilter, filterValue, appliedFilters, data, onApplyFilters, columns]);
@@ -49,7 +26,7 @@ const useFilter = (data, columns, onApplyFilters, initialFilter) => {
       );
       setAppliedFilters(newAppliedFilters);
 
-      const newData = applyFilters(data, newAppliedFilters, columns);
+      const newData = applyFiltersToData(data, newAppliedFilters, columns);
       onApplyFilters(newData);
     },
     [appliedFilters, data, onApplyFilters, columns]
--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,48 @@
+export const applyFiltersToData = (data, appliedFilters, columns) => {
+  return appliedFilters.reduce((filteredData, filter) => {
+    return filteredData.filter((item) => {
+      const operator = filter.value[0];
+      const filterValue = filter.value.slice(1).trim();
+
+      switch (operator) {
+        case '>':
+          return parseFloat(item[filter.type]) > parseFloat(filterValue);
+        case '<':
+          return parseFloat(item[filter.type]) < parseFloat(filterValue);
+        case '!':
+          return !item[filter.type].toString().toLowerCase().includes(filterValue.toLowerCase());
+        default:
+          if (columns.includes(filter.type)) {
+            return item[filter.type].toString().toLowerCase().includes(filter.value.toLowerCase());
+          } else {
+            return true;
+          }
+      }
+    });
+  }, data);
+};
+
 export const convertToCSV = (data, columns) => {
-    const header = columns.join(',');
-    const rows = data.map((row) =>
-      columns.map((columnName) => JSON.stringify(row[columnName])).join(',')
-    );
-    return [header, ...rows].join('\r\n');
-  };
-  
-  export const downloadCSV = (csv, filename) => {
-    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-    const link = document.createElement('a');
-    const url = URL.createObjectURL(blob);
-    link.href = url;
-    link.setAttribute('download', filename);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-  
-  export const getDateTimeStamp = () => {
-    const currentdate = new Date();
-    const datetime = `${currentdate.getFullYear()}-${(currentdate.getMonth()+1).toString().padStart(2, '0')}-${currentdate.getDate().toString().padStart(2, '0')}_${currentdate.getHours().toString().padStart(2, '0')}-${currentdate.getMinutes().toString().padStart(2, '0')}-${currentdate.getSeconds().toString().padStart(2, '0')}`;
-    return datetime;
-  };
-  
+  const header = columns.join(',');
+  const rows = data.map((row) =>
+    columns.map((columnName) => JSON.stringify(row[columnName])).join(',')
+  );
+  return [header, ...rows].join('\r\n');
+};
+
+export const downloadCSV = (csv, filename) => {
+  const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+  const link = document.createElement('a');
+  const url = URL.createObjectURL(blob);
+  link.href = url;
+  link.setAttribute('download', filename);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+export const getDateTimeStamp = () => {
+  const currentdate = new Date();
+  const datetime = `${currentdate.getFullYear()}-${(currentdate.getMonth()+1).toString().padStart(2, '0')}-${currentdate.getDate().toString().padStart(2, '0')}_${currentdate.getHours().toString().padStart(2, '0')}-${currentdate.getMinutes().toString().padStart(2, '0')}-${currentdate.getSeconds().toString().padStart(2, '0')}`;
+  return datetime;
+};
