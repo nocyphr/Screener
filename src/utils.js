07707e5a81d0734763f2a46b6bd73474da2df762
nocyphr <nocyphr@outlook.com>
2023-04-20 17:15:14 +0200

    changed timeformat, added comments, added exports for testability

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,24 @@
-const isGreaterThanThreshold = (item, filterType, filterThreshold) =>
-  parseFloat(item[filterType]) > parseFloat(filterThreshold);
+// Filter utility functions
+export const isGreaterThanThreshold = (item, filterType, filterThreshold) => {
+  return parseFloat(item[filterType]) > parseFloat(filterThreshold);
+};
 
-const isLessThanThreshold = (item, filterType, filterThreshold) =>
-  parseFloat(item[filterType]) < parseFloat(filterThreshold);
+export const isLessThanThreshold = (item, filterType, filterThreshold) => {
+  return parseFloat(item[filterType]) < parseFloat(filterThreshold);
+};
 
-const notIncludesFilter = (item, filterType, filterValue) =>
-  !item[filterType].toString().toLowerCase().includes(filterValue.toLowerCase());
+export const notIncludesFilter = (item, filterType, filterValue) => {
+  return !item[filterType].toString().toLowerCase().includes(filterValue.toLowerCase());
+};
 
-const includesFilter = (item, filterType, filterValue, columns) =>
-  columns.includes(filterType) &&
-  item[filterType].toString().toLowerCase().includes(filterValue.toLowerCase());
+export const includesFilter = (item, filterType, filterValue, columns) => {
+  return (
+    columns.includes(filterType) &&
+    item[filterType].toString().toLowerCase().includes(filterValue.toLowerCase())
+  );
+};
 
+// Applies a single filter to an item
 const applyFilter = (item, filter, columns) => {
   const filterOperator = filter.value[0];
   const filterValue = filter.value.slice(1).trim();
@@ -27,6 +35,7 @@ const applyFilter = (item, filter, columns) => {
   }
 };
 
+// Applies multiple filters to the dataset
 export const applyFiltersToData = (data, appliedFilters, columns) => {
   return appliedFilters.reduce(
     (filteredData, filter) => filteredData.filter((item) => applyFilter(item, filter, columns)),
@@ -34,6 +43,7 @@ export const applyFiltersToData = (data, appliedFilters, columns) => {
   );
 };
 
+// Date and Time formatting utility functions
 const formatDate = (date) => {
   return `${date.getFullYear()}-${(date.getMonth() + 1)
     .toString()
@@ -44,17 +54,19 @@ const formatTime = (date) => {
   return `${date
     .getHours()
     .toString()
-    .padStart(2, '0')}-${date.getMinutes().toString().padStart(2, '0')}-${date
+    .padStart(2, '0')}${date.getMinutes().toString().padStart(2, '0')}${date
     .getSeconds()
     .toString()
     .padStart(2, '0')}`;
 };
 
+// Returns a formatted timestamp
 export const getDateTimeStamp = () => {
   const currentdate = new Date();
   return `${formatDate(currentdate)}_${formatTime(currentdate)}`;
 };
 
+// CSV utility functions
 export const convertToCSV = (data, columns) => {
   const header = columns.join(',');
   const rows = data.map((row) =>
@@ -63,7 +75,7 @@ export const convertToCSV = (data, columns) => {
   return [header, ...rows].join('\r\n');
 };
 
-export const createDownloadLink = (url, filename, document) => {
+const createDownloadLink = (url, filename, document) => {
   const link = document.createElement('a');
   link.href = url;
   link.setAttribute('download', filename);
@@ -75,6 +87,7 @@ export const createDownloadLink = (url, filename, document) => {
   }, 0);
 };
 
+// Downloads CSV file
 export const downloadCSV = (csv, filename, document) => {
   const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
   const url = URL.createObjectURL(blob);
